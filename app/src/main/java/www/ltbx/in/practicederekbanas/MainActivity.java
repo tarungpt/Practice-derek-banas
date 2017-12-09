6b2519a5afbf61dc50933d4838a373e27e1e5ec7
Varun Gupta <varungpt772@gmail.com>
2017-12-10 02:19:09 +0530

    first version complete

--- app/src/main/java/www/ltbx/in/practicederekbanas/MainActivity.java
+++ app/src/main/java/www/ltbx/in/practicederekbanas/MainActivity.java
@@ -2,6 +2,12 @@ package www.ltbx.in.practicederekbanas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] favoriteTVShows = {"Pushing Daisies", "Better Off Ted", "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead", "Breaking Bad", "The 400", "Alphas", "Life on Mars"};
+
+        ListAdapter theAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, favoriteTVShows);
+        ListView theListView = findViewById(R.id.theListView);
+        theListView.setAdapter(theAdapter);
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String tvShowPicked = "You selected " + String.valueOf(adapterView.getItemAtPosition(i));
+                Toast.makeText(MainActivity.this, tvShowPicked, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
